b6bbc5aeb1fc056240648834404f1b2c5cbfc1f0
J.P. Martinez <jpr.martinez@protonmail.com>
2025-04-26 16:53:36 -0700

    Repo cleanup

--- app/ui/panels/combat_tracker_panel_modular/combattrackerpanel_round_helpers.py
+++
@@ -1,7 +0,0 @@
-# Helpers for CombatTrackerPanel (_round_)
-
-    def _round_changed(self, value):
-        """Handle round number change"""
-        self.current_round = value
-        self._update_game_time()
-
--- app/ui/panels/combat_tracker_panel_split/__init__.py
+++
@@ -1,1 +0,0 @@
-from .CombatTrackerPanel import CombatTrackerPanel
